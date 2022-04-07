5e2e197e838406c64ae33b152d4bd482c972c765
Felix Merz <felixmerz@macbook-felix.home>
2022-04-07 19:28:30 +0200

    Initial Bug

---
+++ Ex06Task02/Ex06Task02/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Ex06Task02
+//
+//  Created by Felix Merz on 07.04.22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
